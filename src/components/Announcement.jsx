86b6e3354094944868dc8b4cc982eb25f89bb1ed
charlimora <ca.agudelo.2021@gmail.com>
2024-09-19 11:35:49 -0500

    Creación de estructura de componentes básica del Home y estilización componente Announcement

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './css/Announcement.module.css'
+
+const Announcement = () => {
+  return (
+    <div className={styles.container}>Grandes descuentos de temporada en septiembre</div>
+  )
+}
+
+export default Announcement
